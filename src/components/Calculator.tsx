59a72ac6b7bd32c498f1baa69ba7b281e1e206d6
mehr <mehrfarooq891@gmail.com>
2026-05-31 10:50:23 +0500

    commit cjanges

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -153,7 +153,7 @@ export default function Calculator() {
             </div>
           </div>
 
-          <div className="grid grid-cols-2 gap-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
             <div className="input-group">
               <label className="input-label">Annual Increase (%)</label>
               <input 
